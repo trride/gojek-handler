e5435dfb3b2e6d9f96cffbd42f22315407e695de
Patrick Benz Imanuel <PatrickBenz.09@gmail.com>
2017-10-30 11:28:47 +0700

    booking and cancel

--- test.js
+++ test.js
@@ -2,23 +2,80 @@
 
 require('dotenv').config()
 
-const Gojek = require('./getPrices');
+const Gojek = require('./index');
 
 const config = {
     authorization: process.env.authorization
-}
-
+};
 let start = {
-    lat: '-6.2268253',
-    long: '106.8070988'
-}
-
+    lat: '-7.115222030783675',
+    long: '107.43785351514816'
+};
 let end = {
-    lat: '-6.202178',
-    long: '106.784386'
-}
+    lat: '-7.112052442319981',
+    long: '107.44106613099575'
+};
+let device_token = "";
+let gcm_key = "";
 
 const gojek = new Gojek.GojekHandler(config);
 
-gojek.getMotorBikePrice(start, end)
-.then(console.log);
+async function getPrice (book = false) {
+    let calculatedDetail = await gojek.getCalculateDetail(start, end);
+    const motorBikePriceResult = await gojek.getMotorBikePrice(calculatedDetail.start.origin_lat_long, calculatedDetail.end.destination_lat_long);
+
+    if (!book) {
+        console.log(motorBikePriceResult);
+        return motorBikePriceResult;
+    }
+    return bookRide(calculatedDetail, motorBikePriceResult);
+}
+
+async function bookRide (calculatedDetail, motorBikePriceResult) {
+    const bookingData = await gojek.booking(calculatedDetail.start, calculatedDetail.end, motorBikePriceResult.price.estimate_token, device_token, gcm_key);
+    console.log(bookingData);
+}
+
+getPrice()
+
+async function getPlace () {
+    let test = await gojek.getPlaceNameFromLatLong("-7.115222030783675,107.43785351514816");
+    console.log(test);
+}
+
+// getPlace();
+
+async function cancelList () {
+    let cancelList = await gojek.cancelList();
+    console.log(cancelList);
+}
+
+// cancelList();
+
+async function cancelBook (orderNo) {
+    let cancelResultDetail = await gojek.cancelBooking(orderNo);
+    console.log(cancelResultDetail);
+}
+
+// cancelBook('RB-918208577');
+
+async function getActiveBooking () {
+    let activeBookingDetail = await gojek.getActiveBooking();
+    console.log(activeBookingDetail);
+}
+
+// getActiveBooking();
+
+async function getCurrentBookingByOrderNo (orderNo) {
+    let currentBookingDetail = await gojek.getCurrentBookingByOrderNo(orderNo);
+    console.log(currentBookingDetail);
+}
+
+// getCurrentBookingByOrderNo('RB-918208577');
+
+async function getAcceptedCurrentBookingByOrderNo (orderNo) {
+    let acceptedCurrentBookingDetail = await gojek.getAcceptedCurrentBookingByOrderNo(orderNo);
+    console.log(acceptedCurrentBookingDetail);
+}
+
+// getAcceptedCurrentBookingByOrderNo('RB-918208577');
